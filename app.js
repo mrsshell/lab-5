3896e9763d8605f91bdecff36bd2c99bbd4b9847
mrsshell <mrsshell@comcast.net>
2017-08-11 15:21:22 -0700

    Added the answer to question 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-''use strict';
+'use strict'
 
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the parameters and values that were input/calculated in the function:
@@ -6,7 +6,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
-TODO: Done
+//TODO: Done
 function sum(a,b){
   var result = a + b;
   var output = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
@@ -24,11 +24,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var result = a * b;
+  var output = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result, output];
 }
 
-// TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ multiply(10,6);
+ //testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
